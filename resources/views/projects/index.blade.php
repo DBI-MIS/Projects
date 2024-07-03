f313bc2a30579e2b3598e82e269cebb42516f3ab
DBI-MIS <desktoppublisher@dbiphils.com>
2024-07-03 17:24:10 +0800

    update

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout title="Products">
+<x-app-layout title="Projects">
     <div class="grid grid-cols-5 md:gap-10">
         <div class="md:col-span-5 col-span-5 order-2 md:order-1">
             <livewire:project-list />
